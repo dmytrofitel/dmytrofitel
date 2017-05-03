225e2e15ce87a56feafdc36db966f5acdcf2e9a1
Dmytro Fitel <dimfit@gmail.com>
2017-05-03 18:16:24 +0300

    C:/Users/User103/AppData/Local/Programs/Git/

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,7 @@ $(document).scroll(function(){
 	});
 });
 
-/* Map */	
+/* Map */
 function initMap() {
 	var uluru = {lat: -25.363, lng: 131.044};
 	var map = new google.maps.Map(document.getElementById('map'), {
