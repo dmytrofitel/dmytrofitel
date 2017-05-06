f7fabcbbcc5e3f76b00eaebf9620731b3b674627
Dmytro Fitel <dimfit@gmail.com>
2017-05-06 10:01:02 +0300

    form

--- js/scripts.js
+++ js/scripts.js
@@ -399,3 +399,22 @@ var marker = new google.maps.Marker({
 	map: map
 });
 }
+
+$(function(){
+	$('#send').click(function(e){
+		e.preventDefault();
+		$.ajax({
+			url: "https://formspree.io/dimfit@gmail.com", 
+			method: "POST",
+			data: {
+				name: $('#name').val(),
+				email: $('#email').val(),
+				subject: $('#subject').val(),
+				message: $('#message').val(),
+			},
+			dataType: "json"
+		}).done(function(){
+			$('#form').html('<h2>Thank you for getting in touch!</h2><h4>We appreciate you contacting us about. We try to respond as soon as possible. Have a great day ahead!</h4>');
+		});
+	});
+});
